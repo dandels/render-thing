1d937763d08b56ee1aba8287c718002f47a99cbd
dandels <dandels@hotmail.com>
2025-07-07 11:20:26 +0300

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors)
 app.use(express.json())
 morgan.token('body', function getBody(req) {
     return JSON.stringify(req.body)
